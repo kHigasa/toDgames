6d000a8988c40d794f09e067f9d13f4dcf495c16
kHigasa <kojihigasa@gmail.com>
2018-12-13 22:12:05 +0900

    Initial commit

---
+++ Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+    void Start () {
+        int max = 1000;
+        int min = 1;
+
+        Debug.Log("Welcome to Number Wizard");
+        Debug.Log("Pick two ints, do not tell what it is...");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
